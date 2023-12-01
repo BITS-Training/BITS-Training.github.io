c65c75d4ed958a25fcb51480151f8ecc47332a39
Andreas H <andi@blafasl.de>
2023-12-01 20:05:57 +0100

    test Theme Update

---
+++ en/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "BITS | IT security training for public authorities",
+    "uri": "/en/"
+  },
+  {
+    "breadcrumb": "BITS | IT security training for public authorities",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/en/categories/"
+  },
+  {
+    "breadcrumb": "BITS | IT security training for public authorities",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/en/tags/"
+  }
+]
---
+++ index.search.js
@@ -0,0 +1,58 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": " Updates und Infos zu BITS: www.bits-portal.eu Bundesamt für Sicherheit in der Informationstechnik (Bürgerportal): www.bsi-fuer-buerger.de Netzdurchblick (Internetratgeber für Jugendliche): https://www.netzdurchblick.de ",
+    "description": "",
+    "tags": null,
+    "title": "Weitere Informationen",
+    "uri": "/mehr/weitere-informationen/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "BITS wird herausgegeben von der Kommunal Agentur NRW GmbH und Herrn Dr. Lutz Gollan vom Landesbetrieb Verkehr, Hamburg, und steht unter der Schirmherrschaft des Städte- und Gemeindebundes NRW. Die Urheber sind Herr Dr. Lutz Gollan und Herr Helmut Honermann, PureSec GmbH. Die technische Realisierung erfolgt durch Herrn Andreas Hösl von der Chr. Mayr GmbH + Co. KG.\nBITS ist kostenlos und steht unter der Lizenz Creative Commons BY-SA 4.0. Sie dürfen:\nTeilen — das Material in jedwedem Format oder Medium vervielfältigen und weiterverbreiten Bearbeiten — das Material remixen, verändern und darauf aufbauen und zwar für beliebige Zwecke, sogar kommerziell. Der Lizenzgeber kann diese Freiheiten nicht widerrufen solange Sie sich an folgende Bedingungen halten:\nBedingungen:\nNamensnennung — Sie müssen angemessene Urheber- und Rechteangaben machen, einen Link zur Lizenz beifügen und angeben, ob Änderungen vorgenommen wurden. Diese Angaben dürfen in jeder angemessenen Art und Weise gemacht werden, allerdings nicht so, dass der Eindruck entsteht, der Lizenzgeber unterstütze gerade Sie oder Ihre Nutzung besonders. Weitergabe unter gleichen Bedingungen — Wenn Sie das Material remixen, verändern oder anderweitig direkt darauf aufbauen, dürfen Sie Ihre Beiträge nur unter derselben Lizenz wie das Original - Creative Commons BY-SA 4.0 - verbreiten. Keine weiteren Einschränkungen — Sie dürfen keine zusätzlichen Klauseln oder technische Verfahren einsetzen, die anderen rechtlich irgendetwas untersagen, was die Lizenz erlaubt. Das verwendete hugo-Framework steht unter der Apache-Lizenz, v2.0, das Relearn-Theme, das Quiz inkl. quizdown-js und die jQuery-Bibliothek unter der MIT-Lizenz. Die Icons stammen von https://fontawesome.com und stehen unter freier Lizenz.\nBITS basiert auf open beware!, das von der BDG GmbH \u0026 Co. KG, jetzt NTT Security GmbH, herausgegeben wurde.\n",
+    "description": "",
+    "tags": null,
+    "title": "Herausgeber \u0026 Lizenz",
+    "uri": "/mehr/herausgeber-und-lizenz/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Tastaturnavigation BITS unterstützt die Navigation auch über die Tastatur. Aufzuklappende weiterführende Erläuterungen erfordern aktuell noch eine Mausbedienung.\nSeitennavigation Um zur nächsten Seite zu gelangen, ist die Taste Pfeiltaste nach rechts zu verwenden, um zur vorangehenden Seite zu kommen die Pfeiltaste nach links.\nQuiz Chrome-basierte Browser (Chrome, MS Edge, Vivaldi etc.) Aufgrund eines Bugs in der Version 6.2.x von BITS muss in Chrome-basierten Browsern auf der ersten Seite jedes Quiz’ der Mauszeiger kurz bewegt werden, um die Navigation im Quiz-Block über die Tastatur zu ermöglichen. Dieser wird über die Tabulator-Taste erreicht. Mit den Pfeiltasten (nach oben oder unten) kann dann eine Antwort ausgewählt werden. Anschließend ist über die Tabulator-Taste und die Return-Taste die nächste Aktion auszuwählen bzw. auszulösen. Auf der nächsten Seite muss bei bei gedrückter Umschalt-Taste über die Tabulator-Taste der Antwort-Block angesteuert werden, um über die Pfeiltasten die nächste Antwort zu markieren; im Übrigen geht es weiter wie auf der ersten Seite des Quiz'.\nFirefox Im Browser Firefox ist der Quiz-Block über die Tabulator-Taste anzusteuern. Mit dieser ist auch eine Antwort auszuwählen und über die Leer-Taste zu aktivieren; nach oben kann im Quiz-Block über die gedrückte Umschalt-Taste und die Tabulator-Taste navigiert werden. Anschließend ist erneut über die Tabulator-Taste die nächste Aktion auszuwählen und über die Return-Taste zu bestätigen. Auf der nächsten Seite muss bei bei gedrückter Umschalt-Taste über die Tabulator-Taste der Antwort-Block angesteuert werden; im Übrigen geht es weiter wie auf der ersten Seite des Quiz'.\nSafari Der Safari-Browser bietet aktuell leider keine Möglichkeit, das Quiz per Tastatur zu nutzen.\nKontrast Die Kontrast-Darstellung wurde weitestgehend barrierefrei gestaltet. Im linken Menü kann unten beim Pinsel-Symbol eine besonders deutliche Kontrast-Darstellung ausgewählt werden.\n",
+    "description": "",
+    "tags": null,
+    "title": "Barrierefreiheit",
+    "uri": "/mehr/barrierefreiheit/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Das Impressum für die Online-Version von BITS finden Sie auf https://www.bits-training.de/impressum\nDiese Seite ist Teil der Demo-Version von BITS. Bitte vor der Nutzung anpassen, dies gilt insbesondere für die Platzhalter [XX]!\nInformationen zum Diensteanbieter gemäß § 5 Absatz 1 Telemediengesetz Impressum [Hier eintragen] [Hilfsspalte] Diensteanbieter XX Anschrift XX Rechtsform XX Vertretungsberechtigt XX E-Mail XX Register XX Umsatzsteueridentifikationsnummer XX ",
+    "description": "",
+    "tags": null,
+    "title": "Impressum",
+    "uri": "/mehr/impressum/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Die Datenschutzerklärung für die Online-Version von BITS finden Sie auf https://www.bits-training.de/datenschutz\nDiese Seite ist Teil der Demo-Version von BITS. Bitte vor der Nutzung anpassen, dies gilt insbesondere für die Platzhalter [XX]!\nDatenschutz Verantwortlicher ist [XX], [Anschrift].\nDen/die Datenschutzbeauftragte/n erreichen Sie wie folgt: [XX]\nEs werden folgende Kategorien personenbezogener Daten verarbeitet: [XX]\nDies erfolgt für die Zwecke: [XX].\nDie Rechtsgrundlage hierfür ist Art. 6 Abs. 1 Satz 1 lit.) [XX] Datenschutz-Grundverordnung (DSGVO). [Falls die Rechtsgrundlage lit. f) ist, sind die berechtigten Interessen, z.B. die Bereitstellung der Seiten und deren Sicherheit, zu benennen.]\nEmpfänger Ihrer Daten ist [XX].\nIhre Daten werden für [XX] Tage gespeichert und dann gelöscht.\nDie Verarbeitung der hier genannten Daten ist weder gesetzlich noch vertraglich vorgeschrieben.\nBetroffene Personen haben ein Recht auf Auskunft, Berichtigung, Löschung und die Einschränkung der Verarbeitung. Es besteht das Recht auf Beschwerde bei der nach Art. 77 DSGVO zuständigen Aufsichtsbehörde.\nAuf das Widerspruchsrecht nach Art. 21 DSGVO wird ausdrücklich hingewiesen.\nHosting Unser Hoster benötigt bestimmte Daten, um die Seiten anzuzeigen und verarbeitet in sog. Logfiles folgende Daten, die Ihr Browser übermittelt:\n[XX]\nDas ist erforderlich, um unsere Website anzuzeigen und die Stabilität und Sicherheit zu gewährleisten. Dies entspricht unserem berechtigten Interesse im Sinne des Art. 6 Abs. 1 Satz 1 lit. f) DSGVO.\nDer Hoster ist Empfänger Ihrer personenbezogenen Daten. Sie werden nach [XX] Tagen gelöscht.\nTracking [XX]\n",
+    "description": "",
+    "tags": null,
+    "title": "Datenschutz",
+    "uri": "/mehr/datenschutz/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/"
+  }
+]
