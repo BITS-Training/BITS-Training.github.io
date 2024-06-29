034b5d0911275757cf1f83455dcfd70ee9243789
Andreas H <andi@blafasl.de>
2024-06-29 15:35:22 +0200

    BITS v6.5.0

---
+++ training/en/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "BITS | IT security training for public authorities",
+    "uri": "/en/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/en/categories/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/en/tags/"
+  }
+]
